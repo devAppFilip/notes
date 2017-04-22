a4492c289fe0701c536e5ebb4a9d1b19a35f7948
Filip Lesnjakovic <filip.lesnjakovic@gmail.com>
2017-04-22 11:07:01 +0200

    Integrated selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,16 @@
+export const notes = [
+  {
+    _id: 'noteId1',
+    title: 'Test Title',
+    body: '',
+    updatedAt: 1492803620932,
+    userId: 'userId1'
+  },
+  {
+    _id: 'noteId2',
+    title: '',
+    body: 'Some body',
+    updatedAt: 1492803620932,
+    userId: 'userId2'
+  }
+];
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -4,33 +4,42 @@ import { mount } from 'enzyme';
 import { Meteor } from 'meteor/meteor';
 
 import { NoteListItem } from './NoteListItem';
+import { notes } from '../fixtures/fixtures';
 
 if(Meteor.isClient){
 
   describe('NoteListItem', function() {
+    let Session;
+
+    beforeEach(() => {
+      Session = {
+        set: expect.createSpy()
+      };
+    });
 
     it('should show title in h5', function() {
-      const noteOne = {
-        _id: 'testNoteId1',
-        title: 'My Title',
-        body: 'My body for note',
-        updatedAt: 1492803620932,
-        userId: 'testUserId1'
-      };
 
-      const wrapper = mount(<NoteListItem note={noteOne}/>);
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
       const h5Text = wrapper.find('h5').text();
       const timestamp = wrapper.find('p').text();
 
-      expect(h5Text).toBe(noteOne.title);
+      expect(h5Text).toBe(notes[0].title);
       expect(timestamp).toBe('21/4/17');
+
     });
 
     it('should show default title if no title set', function() {
       const updatedAt = 1492803620932;
-      const wrapper = mount(<NoteListItem note={{title: '', updatedAt}}/>);
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
 
       expect(wrapper.find('h5').text()).toBe('Untitled note');
+    });
+
+    it('should call set on click', function() {
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
+      wrapper.find('div').simulate('click');
+
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[1]._id);
     });
 
   });
